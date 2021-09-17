0520090d8fad76125f55d92e4d12bef404a3ee89
Kimjimin0807 <jm004471@naver.com>
2021-09-18 03:42:38 +0900

    ⚡️ :: (#3) 로그인 페이지 경로 설정

--- rms_admin/src/route/MainRouter.tsx
+++ rms_admin/src/route/MainRouter.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  LoginContainer
+} from '../containers';
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
-          <Route  />
+          <Route  path="/login" component={LoginContainer}/>
       </Switch>
     </BrowserRouter>
   );
