973917dbd50f047d1326e7b92b647c4430c19648
Kimjimin0807 <jm004471@naver.com>
2021-09-20 00:19:10 +0900

    ⚡️ :: (#6) 무한스크롤 PageNation으로 변경

--- rms_admin/src/components/viewReportList/ViewReportList.tsx
+++ rms_admin/src/components/viewReportList/ViewReportList.tsx
@@ -9,12 +9,23 @@ const ViewReportList: FC = () => {
         <S.ReportListWrapper>
           <S.TitleFont>승인 요청된 보고서</S.TitleFont>
           <S.ReportListContainer>
-            {Array(10)
+            {Array(5)
               .fill(0)
               .map((v, i) => {
-                return <Report key={i}/>;
+                return <Report key={i} />;
               })}
           </S.ReportListContainer>
+          <S.PageNationWrapper>
+            <S.PageNationButton>{"<"}</S.PageNationButton>
+            {Array(5)
+              .fill(0)
+              .map((v, i) => {
+                return (
+                  <S.PageNationButton key={i + 1}>{i + 1}</S.PageNationButton>
+                );
+              })}
+            <S.PageNationButton>{">"}</S.PageNationButton>
+          </S.PageNationWrapper>
         </S.ReportListWrapper>
       </S.ViewReportList>
     </>
