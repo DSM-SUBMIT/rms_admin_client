c0a7040ff553b56b5e169d903fd7d494bdf0b52f
Kimjimin0807 <jm004471@naver.com>
2021-09-19 20:18:41 +0900

    ⚡️ :: (#6) viewReportList route 설정

--- rms_admin/src/route/MainRouter.tsx
+++ rms_admin/src/route/MainRouter.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
-  LoginContainer, ViewPlanListContainer
+  LoginContainer, ViewPlanListContainer, ViewReportListContainer
 } from '../containers';
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
-          <Route  path="/login" component={LoginContainer}/>
-          <Route path="/view/planList" component={ViewPlanListContainer}/>
+          <Route exact path="/login" component={LoginContainer}/>
+          <Route exact path="/view/planList" component={ViewPlanListContainer}/>
+          <Route exact path="/view/reportList" component={ViewReportListContainer}/>
       </Switch>
     </BrowserRouter>
   );
