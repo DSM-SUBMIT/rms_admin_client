42c77dac8c959f62e3711e34255a056bd0099176
yelim <choyr2016@naver.com>
2021-09-19 18:20:07 +0900

    ⚡️ :: (#15) 헤더 퍼블리싱

--- rms_admin/src/asset/index.ts
+++ rms_admin/src/asset/index.ts
@@ -1,2 +1,3 @@
+export {default as profile} from './header/profile.svg';
 export {default as id} from './login/id.svg';
 export {default as password} from './login/password.svg';
---
+++ rms_admin/src/components/header/style.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+    width: 100%;
+    height: 55px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Center = styled.div`
+    width: 800px;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const Logo = styled.div`
+    
+`
+
+export const Nav = styled.nav`
+    width: 340px;
+    display: flex;
+    justify-content: space-around;
+`
+
+export const Plans = styled.span`
+`
+
+export const Reports = styled.span`
+`
+
+export const Projects = styled.span`
+`
+
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+    img {
+        width: 15px;
+    }
+    span {
+        margin-left: 3px;
+    }
+`
