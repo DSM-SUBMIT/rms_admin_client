9fb0e1c8a4c0eaf105604b31fdeec8f2a53b238c
Kimjimin0807 <jm004471@naver.com>
2021-09-27 00:30:11 +0900

    ⚡️ :: (#7) Feedback Modal 추가

--- rms_admin/src/containers/viewPlan/ViewPlan.tsx
+++ rms_admin/src/containers/viewPlan/ViewPlan.tsx
@@ -1,5 +1,6 @@
 import React, { FC, Suspense } from "react";
 import { ViewPlan } from "../../components";
+import FeedbackModal from "../../components/modal/feedback";
 
 const ViewPlanContainer: FC = () => {
   return (
