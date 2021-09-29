a534a9be43c18da42706b3347ad2efe3df808c30
yelim <choyr2016@naver.com>
2021-09-29 10:36:47 +0900

    ⚡️ :: (#10) categoryBox mapping

--- rms_admin/src/components/viewProjectList/category/Category.tsx
+++ rms_admin/src/components/viewProjectList/category/Category.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import * as S from '../style'
+import { CategoryBox } from '../../../constance/category';
 
 const Category: FC = () => {
     return (
@@ -7,30 +8,17 @@ const Category: FC = () => {
         <S.CategoryArea>
         <h3>분야 선택</h3>
         <S.Category>
-            <div>
-                <input type="checkbox" id="web"/>
-                <label htmlFor="web">웹</label>
-            </div>
-            <div>
-                <input type="checkbox" id="app"/>
-                <label htmlFor="app">앱</label>
-            </div>
-            <div>
-                <input type="checkbox" id="game"/>
-                <label htmlFor="game">게임</label>
-            </div>
-            <div>
-                <input type="checkbox" id="embedded"/>
-                <label htmlFor="embedded">임베디드</label>
-            </div>
-            <div>
-                <input type="checkbox" id="security"/>
-                <label htmlFor="security">보안</label>
-            </div>
-            <div>
-                <input type="checkbox" id="ai"/>
-                <label htmlFor="ai">인공지능 / 빅데이터</label>
-            </div>
+            {
+                CategoryBox
+                .map((v, i) => {
+                    return(
+                        <div>
+                            <input type="checkbox" id={v.id}/>
+                            <label htmlFor={v.id}>{v.span}</label>
+                        </div>
+                    )
+                })
+            }
         </S.Category>
     </S.CategoryArea>
     );
