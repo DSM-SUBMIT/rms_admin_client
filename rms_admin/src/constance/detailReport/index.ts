206b3c2c5525704d195cdbd9887202a929e621a0
hyejun <hyejun0608@naver.com>
2021-09-30 23:17:12 +0900

    ⚡️ :: (#28) detail report constance

--- rms_admin/src/constance/detailReport/index.ts
+++ rms_admin/src/constance/detailReport/index.ts
@@ -4,4 +4,9 @@ export const SUBMITTER = '제출자';
 export const TEACHER = '담당교사 확인';
 export const SCHOOL = '대덕소프트웨어마이스터고등학교';
 export const SUBTITLE = '보고서 작성 요령';
-export const DOWNLOAD = '다운로드';
+export const VIDEODOWNLOAD = '시연 영상 다운로드';
+export const BTN = [
+  { id: 'plan', content: '계획서 보기' },
+  { id: 'approve', content: '승인/미승인' },
+  { id: 'download', content: '다운로드' },
+];
