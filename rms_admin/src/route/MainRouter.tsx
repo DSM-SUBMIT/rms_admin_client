b86c6011e46eea0146da93cb195d80c55dc445ff
hyejun <hyejun0608@naver.com>
2021-09-30 23:35:47 +0900

    ⚡️ :: (#28) detail report container 추가

--- rms_admin/src/route/MainRouter.tsx
+++ rms_admin/src/route/MainRouter.tsx
@@ -1,41 +1,27 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   LoginContainer,
   ViewPlanContainer,
   ViewPlanListContainer,
   ViewReportListContainer,
   SearchProjectListContainer,
-  ViewProjectListContainer
-} from "../containers";
+  ViewProjectListContainer,
+  DetailReportContainer,
+} from '../containers';
 
 const MainRouter = () => {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/login" component={LoginContainer} />
-          <Route
-            exact
-            path="/view/planList"
-            component={ViewPlanListContainer}
-          />
-          <Route
-            exact
-            path="/view/reportList"
-            component={ViewReportListContainer}
-          />
-          <Route exact path="/view/plan" component={ViewPlanContainer} />
-          <Route
-            exact
-            path="/search"
-            component={SearchProjectListContainer}
-          />
-          <Route
-            exact
-            path="/view/projectList"
-            component={ViewProjectListContainer}
-          />
+          <Route exact path='/login' component={LoginContainer} />
+          <Route exact path='/view/planList' component={ViewPlanListContainer} />
+          <Route exact path='/view/reportList' component={ViewReportListContainer} />
+          <Route exact path='/view/plan' component={ViewPlanContainer} />
+          <Route exact path='/search' component={SearchProjectListContainer} />
+          <Route exact path='/view/projectList' component={ViewProjectListContainer} />
+          <Route exact path='/detail-report/:id' component={DetailReportContainer} />
         </Switch>
       </BrowserRouter>
     </>
