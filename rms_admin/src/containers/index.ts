c1246e76634a194b6242e014c28e7def228fbe92
hyejun <hyejun0608@naver.com>
2021-09-30 22:57:49 +0900

    ⚡️ :: (#28) detail report container 추가

--- rms_admin/src/containers/index.ts
+++ rms_admin/src/containers/index.ts
@@ -1,7 +1,8 @@
-export { default as LoginContainer } from "./login";
-export { default as ViewPlanListContainer } from "./viewPlanList";
-export { default as ViewReportListContainer } from "./viewReportList";
+export { default as LoginContainer } from './login';
+export { default as ViewPlanListContainer } from './viewPlanList';
+export { default as ViewReportListContainer } from './viewReportList';
 export { default as ViewPlanContainer } from './viewPlan';
 // export { default as ModalContainer } from "./modal";
-export { default as SearchProjectListContainer } from "./searchProjectList";
-export { default as ViewProjectListContainer } from "./viewProjectList";
+export { default as SearchProjectListContainer } from './searchProjectList';
+export { default as ViewProjectListContainer } from './viewProjectList';
+export { default as DetailReportContainer } from './detailReport';
