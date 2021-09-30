687b6d7eecf857f3852517f5bd57b21eb1325ce7
hyejun <hyejun0608@naver.com>
2021-09-30 23:34:56 +0900

    ⚡️ :: (#28) close btn click handler

--- rms_admin/src/components/modal/feedback/Feedback.tsx
+++ rms_admin/src/components/modal/feedback/Feedback.tsx
@@ -1,15 +1,27 @@
-import React, { FC } from "react";
-import { closeModalIcon } from "../../../asset";
-import * as S from "./style";
+import React, { FC } from 'react';
+import { closeModalIcon } from '../../../asset';
+import * as S from './style';
 
-const FeedbackModal: FC = () => {
+interface Props {
+  setClose: (value: React.SetStateAction<boolean>) => void;
+}
+
+const FeedbackModal: FC<Props> = ({ setClose }) => {
+  const closeModalClickHandler = () => {
+    setClose(false);
+  };
+
   return (
     <>
       <S.ModalWrapper>
         <S.Modal>
-          <S.CloseModalIcon src={closeModalIcon} alt="closeModal" />
+          <S.CloseModalIcon
+            src={closeModalIcon}
+            alt='closeModal'
+            onClick={closeModalClickHandler}
+          />
           <S.TitleFont>해당 보고서의 피드백 내용을 적어주세요</S.TitleFont>
-          <S.FeedbackInput/>
+          <S.FeedbackInput />
           <S.CheckButtonWrapper>
             <S.CheckButton>미승인</S.CheckButton>
             <S.CheckButton>승인</S.CheckButton>
