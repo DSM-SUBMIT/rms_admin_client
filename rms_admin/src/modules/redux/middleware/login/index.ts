6388ba889700c0ec1318ec36c07a747867c88742
Kimjimin0807 <jm004471@naver.com>
2021-10-02 04:09:21 +0900

    ⚡️ :: (#26) login saga 추가

---
+++ rms_admin/src/modules/redux/middleware/login/index.ts
@@ -0,0 +1,44 @@
+import { call, put, select, takeLatest } from "redux-saga/effects";
+import { TOKEN } from "../../action/login/interface";
+import { getToken } from "../../../../util/api/login";
+import { reducerType } from "../../reducer";
+import LoginState from "../../reducer/login/interface";
+
+const getStateFunc = (state: reducerType): LoginState => state.login;
+
+export const loginRequestSaga = function* (): any {
+  const SUCCESS = "LOGIN/SUCCESS";
+  const FAILURE = "LOGIN/FAILURE";
+  const state = yield select(getStateFunc);
+
+  try {
+    console.log(state);
+    const response = yield call(getToken, state);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data : null,
+    });
+  } catch (error: any) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: "LOGIN/TOLEN" },
+      });
+    } else {
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: `Network Error`,
+          status: 500,
+        },
+      });
+    }
+  }
+};
+
+
+function* loginSaga() {
+  yield takeLatest(TOKEN, loginRequestSaga);
+}
+
+export default loginSaga;
