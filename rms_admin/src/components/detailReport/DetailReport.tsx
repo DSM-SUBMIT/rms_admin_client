7fcf06749fe311fc7f148e33109cbf5a841109ce
hyejun <hyejun0608@naver.com>
2021-10-01 09:55:16 +0900

    ⚡️ :: (#28) 피드백 모달 삭제

--- rms_admin/src/components/detailReport/DetailReport.tsx
+++ rms_admin/src/components/detailReport/DetailReport.tsx
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as S from './style';
 import Header from '../header/Header';
 import ReportFirstPage from './ReportFirstPage';
 import WritedReport from './WritedReport';
 import { BTN, VIDEODOWNLOAD } from '../../constance/detailReport';
 import { video } from '../../asset/detailReport';
-import FeedbackModal from '../modal/feedback';
 
 const DetailReport = () => {
-  const [isFeedbackModalClick, setIsFeedbackModalClick] = useState<boolean>(false);
-
-  const feedbackBtnClickHandler = () => {
-    setIsFeedbackModalClick(true);
-  };
-
   return (
     <S.DetailReport>
-      {isFeedbackModalClick && <FeedbackModal setClose={setIsFeedbackModalClick} />}
       <Header />
       <S.Pages>
         <ReportFirstPage />
@@ -24,18 +16,19 @@ const DetailReport = () => {
         <WritedReport />
       </S.Pages>
       <S.BtnLine>
-        <S.VideoImg src={video} />
-        <S.VideoDownload>{VIDEODOWNLOAD}</S.VideoDownload>
-        {BTN.map(data => {
-          return (
-            <S.Btn
-              key={data.id}
-              onClick={data.id === 'approve' ? feedbackBtnClickHandler : () => {}}
-            >
-              <p>{data.content}</p>
-            </S.Btn>
-          );
-        })}
+        <div>
+          <S.VideoImg src={video} />
+          <S.VideoDownload>{VIDEODOWNLOAD}</S.VideoDownload>
+        </div>
+        <div>
+          {BTN.map(data => {
+            return (
+              <S.Btn key={data.id}>
+                <p>{data.content}</p>
+              </S.Btn>
+            );
+          })}
+        </div>
       </S.BtnLine>
     </S.DetailReport>
   );
