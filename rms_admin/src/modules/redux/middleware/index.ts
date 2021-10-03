d19cc4adb747fb3f3a65cf87b8db4ed81af5ea31
Kimjimin0807 <jm004471@naver.com>
2021-10-04 05:48:17 +0900

    ⚡️ :: (#27) changePassword saga 추가

--- rms_admin/src/modules/redux/middleware/index.ts
+++ rms_admin/src/modules/redux/middleware/index.ts
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
+import changePasswordSaga from './changePassword';
 import loginSaga from './login';
 import planListSaga from './planList';
 
 export default function* rootSaga() {
-  yield all([loginSaga(), planListSaga()]);
+  yield all([loginSaga(), planListSaga(), changePasswordSaga()]);
 }
