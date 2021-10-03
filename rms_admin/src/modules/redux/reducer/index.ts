b9c94410f63035e25ebd00fd1e592069701b7b60
Kimjimin0807 <jm004471@naver.com>
2021-10-04 05:44:09 +0900

    ⚡️ :: (#27) changePassword reducer 추가

--- rms_admin/src/modules/redux/reducer/index.ts
+++ rms_admin/src/modules/redux/reducer/index.ts
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
+import changePasswordReducer from './changePassword';
 import loginReducer from './login';
+import modalReducer from './modal';
 import planListReducer from './planList';
 
-const rootReducer = combineReducers({ login: loginReducer, planList: planListReducer });
+const rootReducer = combineReducers({
+  login: loginReducer,
+  planList: planListReducer,
+  modal: modalReducer,
+  chagePassword: changePasswordReducer,
+});
 
 export type reducerType = ReturnType<typeof rootReducer>;
 export default rootReducer;
