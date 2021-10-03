1cbba9984eade878a7437ff75a351ea8b5c2871e
Kimjimin0807 <jm004471@naver.com>
2021-10-04 07:22:32 +0900

    ⚡️ :: (#38) reportlist saga 추가

--- rms_admin/src/modules/redux/middleware/index.ts
+++ rms_admin/src/modules/redux/middleware/index.ts
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 import loginSaga from './login';
 import planListSaga from './planList';
+import reportListSaga from './reportList';
 
 export default function* rootSaga() {
-  yield all([loginSaga(), planListSaga()]);
+  yield all([loginSaga(), planListSaga(), reportListSaga()]);
 }
