2b562370eec4d89e70869c4d68add3aad0e6c008
hyejun <hyejun0608@naver.com>
2021-10-04 02:05:37 +0900

    ⚡️ :: (#28) approve detail report container

--- rms_admin/src/containers/detailReport/ApproveDetailReportContainer.tsx
+++ rms_admin/src/containers/detailReport/ApproveDetailReportContainer.tsx
@@ -1,10 +1,27 @@
-import React, { FC, Suspense } from 'react';
+import React, { FC, Suspense, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { ApproveDetailReport } from '../../components/detailReport';
+import { GET_REPORT } from '../../modules/redux/action/detailReport/interface';
+import UseDetailReoprt from '../../util/hooks/detailReport/UseDetailReport';
 
 const ApproveDetailReportContainer: FC = () => {
+  const { state, setState } = UseDetailReoprt();
+  const dispatch = useDispatch();
+
+  const path = useLocation().pathname.slice(20);
+
+  useEffect(() => {
+    setState.setId(path);
+  }, [path]);
+
+  useEffect(() => {
+    dispatch({ type: GET_REPORT });
+  }, []);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <ApproveDetailReport />
+      <ApproveDetailReport {...state} {...setState} />
     </Suspense>
   );
 };
