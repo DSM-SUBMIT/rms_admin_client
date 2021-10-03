11bc3de66cd7746fe5ae23fbe990168958b75d61
hyejun <hyejun0608@naver.com>
2021-10-04 02:03:51 +0900

    ⚡️ :: (#28) detailReportReducer 추가

--- rms_admin/src/modules/redux/reducer/index.ts
+++ rms_admin/src/modules/redux/reducer/index.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import loginReducer from './login';
+import detailReportReducer from './detailReport';
 
-const rootReducer = combineReducers({ login: loginReducer });
+const rootReducer = combineReducers({ login: loginReducer, detailReport: detailReportReducer });
 
 export type reducerType = ReturnType<typeof rootReducer>;
 export default rootReducer;
