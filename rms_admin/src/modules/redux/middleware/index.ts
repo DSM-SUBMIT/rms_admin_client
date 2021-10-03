c809cf836e70d2ecacb47925bba70455d42d92cc
hyejun <hyejun0608@naver.com>
2021-10-04 02:04:42 +0900

    ⚡️ :: (#28) detailReportSaga 추가

--- rms_admin/src/modules/redux/middleware/index.ts
+++ rms_admin/src/modules/redux/middleware/index.ts
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import loginSaga from './login';
+import detailReportSaga from './detailReport';
 
 export default function* rootSaga() {
-  yield all([loginSaga()]);
+  yield all([loginSaga(), detailReportSaga()]);
 }
