e579295cf3387ec030a0dbf3aaa43b0b6a67322c
Kimjimin0807 <jm004471@naver.com>
2021-10-04 20:31:10 +0900

    ⚡️ :: (#40) planDetail action 추가

---
+++ rms_admin/src/modules/redux/action/plan/index.ts
@@ -0,0 +1,5 @@
+import { createAction } from "typesafe-actions";
+import { GET_PLAN } from "./interface";
+
+
+export const getPlan = createAction(GET_PLAN)();
---
+++ rms_admin/src/modules/redux/action/plan/interface/index.ts
@@ -0,0 +1,3 @@
+export const GET_PLAN = 'PLAN/GET_PLAN' as const;
+export const GET_PLAN_SUCCESS = 'PLAN/GET_PLAN_SUCCESS' as const;
+export const GET_PLAN_FAILURE = 'PLAN/GET_PLAN_FAILURE' as const;
