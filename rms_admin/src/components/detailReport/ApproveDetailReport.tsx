d9816590d10cccb9e1c4883c35231091be22fa73
hyejun <hyejun0608@naver.com>
2021-10-06 10:51:57 +0900

    ⚡️ :: (#28) 다운로드 버튼을 누르면 다운로드가 됨

--- rms_admin/src/components/detailReport/ApproveDetailReport.tsx
+++ rms_admin/src/components/detailReport/ApproveDetailReport.tsx
@@ -6,8 +6,12 @@ import WritedReport from './WritedReport';
 import { APPROVEBTN, VIDEODOWNLOAD } from '../../constance/detailReport';
 import { video } from '../../asset/detailReport';
 import FeedbackModal from '../modal/feedback';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import { useHistory } from 'react-router-dom';
 
 interface Props {
+  id: string;
   content: string;
   field: Array<string>;
   writer: string;
@@ -15,8 +19,9 @@ interface Props {
 }
 
 const ApproveDetailReport: FC<Props> = props => {
-  const { content, field, writer, videoUrl } = props;
+  const { content, field, writer, videoUrl, id } = props;
   const [isFeedbackModalClick, setIsFeedbackModalClick] = useState<boolean>(false);
+  const history = useHistory();
 
   const feedbackBtnClickHandler = () => {
     setIsFeedbackModalClick(true);
@@ -26,11 +31,41 @@ const ApproveDetailReport: FC<Props> = props => {
     window.location.href = videoUrl;
   };
 
+  const viewPlanBtnClickHandler = () => {
+    history.push(`/view/plan/${id}`);
+  };
+
+  const downloadBtnClickHandler = () => {
+    (document.getElementById('pdf') as HTMLElement).style.padding = '0px';
+    html2canvas(document.getElementById('pdf') as HTMLElement).then(canvas => {
+      let imgData = canvas.toDataURL('image/png');
+      let pdf = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      let imgWidth = 210;
+      let pageHeight = imgWidth * 1.414;
+      let imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 20) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+
+      pdf.save('report.pdf');
+    });
+    (document.getElementById('pdf') as HTMLElement).style.paddingTop = '54px';
+  };
+
   return (
     <S.DetailReport>
-      {isFeedbackModalClick && <FeedbackModal setClose={setIsFeedbackModalClick} />}
+      {isFeedbackModalClick && <FeedbackModal type={'report'} setClose={setIsFeedbackModalClick} />}
       <Header />
-      <S.Pages>
+      <S.Pages id='pdf'>
         <ReportFirstPage field={field} writer={writer} />
         <WritedReport isSecondPage content={content} />
         <WritedReport content={content} />
@@ -45,7 +80,13 @@ const ApproveDetailReport: FC<Props> = props => {
             return (
               <S.Btn
                 key={data.id}
-                onClick={data.id === 'approve' ? feedbackBtnClickHandler : () => {}}
+                onClick={
+                  data.id === 'approve'
+                    ? feedbackBtnClickHandler
+                    : data.id === 'download'
+                    ? downloadBtnClickHandler
+                    : viewPlanBtnClickHandler
+                }
               >
                 <p>{data.content}</p>
               </S.Btn>
