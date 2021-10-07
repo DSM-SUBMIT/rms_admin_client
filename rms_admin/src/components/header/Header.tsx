2bf412938fd6a365204c54a86cbf352e8c94aa75
Kimjimin0807 <jm004471@naver.com>
2021-10-07 23:24:07 +0900

    ⚡️ :: (#26) 토큰 만료시 리프레시 토큰시작

--- rms_admin/src/components/header/Header.tsx
+++ rms_admin/src/components/header/Header.tsx
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as S from './style';
-import { profile } from '../../asset'
+import { profile } from '../../asset';
 import { useModal } from '../../util/hooks/modal';
+import { useLogin } from '../../util/hooks/login';
+import { REFRESH_TOKEN } from '../../modules/redux/action/login/interface';
+import moment from 'moment';
+import { useHistory } from 'react-router';
 
 const Header = () => {
-    const {setState} = useModal();
+  const loginState = useLogin();
+  const { setState } = useModal();
+  const history = useHistory();
 
-    const onClickChangePasswordModal = () => {
-        setState.setModalOn('changePassword');
+  useEffect(() => {
+    if (
+      moment(localStorage.getItem('access_token_expiration_time')).diff(moment()) < 0 &&
+      localStorage.getItem('refresh_token')
+    ) {
+      loginState.setState.refresh_token();
     }
+  });
 
-    return (
-        <S.Header>
-            <S.Center>
-                <S.Logo>RMS</S.Logo>
-                <S.Nav>
-                    <span>계획서</span>
-                    <span>보고서</span>
-                    <span>승인된 프로젝트</span>
-                    <S.Profile onClick={onClickChangePasswordModal}>
-                        <img src={profile} alt="profile"/>
-                        <span>비밀번호 변경</span>
-                    </S.Profile>
-                </S.Nav>
-            </S.Center>
-        </S.Header>
-    );
+  useEffect(() => {
+    if (
+      loginState.state.error?.statusCode === 401 &&
+      loginState.state.error.type === REFRESH_TOKEN
+    ) {
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('refresh_token');
+      history.push('/');
+    }
+  }, [loginState.state.error]);
+
+  const onClickChangePasswordModal = () => {
+    setState.setModalOn('changePassword');
+  };
+
+  return (
+    <S.Header>
+      <S.Center>
+        <S.Logo>RMS</S.Logo>
+        <S.Nav>
+          <span>계획서</span>
+          <span>보고서</span>
+          <span>승인된 프로젝트</span>
+          <S.Profile onClick={onClickChangePasswordModal}>
+            <img src={profile} alt='profile' />
+            <span>비밀번호 변경</span>
+          </S.Profile>
+        </S.Nav>
+      </S.Center>
+    </S.Header>
+  );
 };
 
 export default Header;
