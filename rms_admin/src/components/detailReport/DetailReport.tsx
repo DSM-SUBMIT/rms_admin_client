f7bfd91c5680d952d39c142e6b314e5137b1be0f
hyejun <hyejun0608@naver.com>
2021-10-08 13:34:44 +0900

    ⚡️ :: (#28) props로 state를 전달해줌

--- rms_admin/src/components/detailReport/DetailReport.tsx
+++ rms_admin/src/components/detailReport/DetailReport.tsx
@@ -1,29 +1,32 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import * as S from './style';
 import Header from '../header/Header';
 import ReportFirstPage from './ReportFirstPage';
 import WritedReport from './WritedReport';
-import { BTN, VIDEODOWNLOAD } from '../../constance/detailReport';
+import { BTN, DOWNLOADING, VIDEODOWNLOAD } from '../../constance/detailReport';
 import { video } from '../../asset/detailReport';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import { useHistory, useLocation } from 'react-router-dom';
+import { getDownloadFile } from '../../util/api/detailReport';
 
 interface Props {
   content: string;
   projectName: string;
   writer: string;
-  videoUrl: string;
+  isIndividual: boolean;
+  writerStdNumber: number;
 }
 
 const DetailReport: FC<Props> = props => {
-  const { content, projectName, writer, videoUrl } = props;
+  const { content, projectName, writer, isIndividual, writerStdNumber } = props;
+  const [isDownloading, setIsDownloading] = useState<boolean>(false);
   const history = useHistory();
   const id = useLocation().pathname.slice(20);
 
   const videoDownloadBtnClickHandler = () => {
-    const videoPage = window.open('about:blank');
-    (videoPage as Window).location.href = videoUrl;
+    getDownloadFile(id, projectName);
+    setIsDownloading(true);
   };
 
   const makeContentArray = useMemo(() => {
@@ -66,7 +69,12 @@ const DetailReport: FC<Props> = props => {
     <S.DetailReport>
       <Header />
       <S.Pages id='pdf'>
-        <ReportFirstPage projectName={projectName} writer={writer} />
+        <ReportFirstPage
+          projectName={projectName}
+          writer={writer}
+          isIndividual={isIndividual}
+          writerStdNumber={writerStdNumber}
+        />
         {makeContentArray.map((data: string, id: number) => {
           return <WritedReport isSecondPage={id === 0 ? true : false} content={data} />;
         })}
@@ -74,7 +82,15 @@ const DetailReport: FC<Props> = props => {
       <S.BtnLine>
         <div>
           <S.VideoImg src={video} onClick={videoDownloadBtnClickHandler} />
-          <S.VideoDownload onClick={videoDownloadBtnClickHandler}>{VIDEODOWNLOAD}</S.VideoDownload>
+          <S.VideoDownload onClick={videoDownloadBtnClickHandler}>
+            {VIDEODOWNLOAD}
+            {isDownloading && (
+              <S.DownloadingMessage>
+                <br />
+                {DOWNLOADING}
+              </S.DownloadingMessage>
+            )}
+          </S.VideoDownload>
         </div>
         <div>
           {BTN.map(data => {
