2e63129dbb81925214fa393db574b92dd5dcd213
Kimjimin0807 <jm004471@naver.com>
2021-10-08 23:14:00 +0900

    ⚡️ :: (#40) 계획서 상세보기 saga 추가

--- rms_admin/src/modules/redux/middleware/index.ts
+++ rms_admin/src/modules/redux/middleware/index.ts
@@ -1,9 +1,16 @@
 import { all } from 'redux-saga/effects';
 import changePasswordSaga from './changePassword';
 import loginSaga from './login';
+import detailPlanSaga from './plan';
 import planListSaga from './planList';
 import reportListSaga from './reportList';
 
 export default function* rootSaga() {
-  yield all([loginSaga(), planListSaga(), changePasswordSaga(), reportListSaga()]);
+  yield all([
+    loginSaga(),
+    planListSaga(),
+    changePasswordSaga(),
+    reportListSaga(),
+    detailPlanSaga(),
+  ]);
 }
