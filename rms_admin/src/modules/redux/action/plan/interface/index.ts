d25be71de90ede4539fd01f14a991492efbc1cd7
Kimjimin0807 <jm004471@naver.com>
2021-10-08 23:13:23 +0900

    ⚡️ :: (#40) 계획서 상세보기 action

--- rms_admin/src/modules/redux/action/plan/interface/index.ts
+++ rms_admin/src/modules/redux/action/plan/interface/index.ts
@@ -1,3 +1,4 @@
-export const GET_PLAN = 'PLAN/GET_PLAN' as const;
-export const GET_PLAN_SUCCESS = 'PLAN/GET_PLAN_SUCCESS' as const;
-export const GET_PLAN_FAILURE = 'PLAN/GET_PLAN_FAILURE' as const;
+export const GET_DETAIL_PLAN = 'PLAN/GET_DETAIL_PLAN' as const;
+export const GET_DETAIL_PLAN_SUCCESS = 'PLAN/GET_DETAIL_PLAN_SUCCESS' as const;
+export const GET_DETAIL_PLAN_FAILURE = 'PLAN/GET_DETAIL_PLAN_FAILURE' as const;
+export const ID = 'PLAN/ID' as const;
